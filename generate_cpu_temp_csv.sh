b1b20a047b45d37248f7043c1bfef953648c464e
Rheik van Eyck <rheikvaneyck@yahoo.de>
2013-12-27 12:05:13 +0100

    Create generate_cpu_temp_csv.sh

---
+++ generate_cpu_temp_csv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Tabelle als csv ausgeben und
+# Zeitstempel in lokale Zeit konvertieren
+
+# CSV-Datei 
+if [[ $# -lt 2 ]]; then
+  DIRCSV=/var/log
+  CSV=$DIRCSV/status.csv
+else
+  CSV=$2
+fi
+
+echo "Lese Daten von $CSV" 
+  
+if [ ! -f "$2" ]; then
+    echo "Kann die Datenbank $2 nicht finden"
+    exit
+fi
+
+# Die Tabelle stats enthält die Spalten: id, ts, temp
+sqlite3 -separator ',' -csv  -header $DB "SELECT id AS "ID",datetime(ts, 'unixepoch', 'localtime') AS "Zeit", temp AS "Temperatur" FROM stats;" > $CSV
