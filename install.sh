8e1b448018a286e4d849fb1535730f5ba26e6f98
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-01-20 12:27:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,16 @@ LOGDIR="$APPDIR/log"
 [ ! -d "$BINDIR" ] && mkdir -p "$BINDIR"
 [ ! -d "$LOGDIR" ] && mkdir -p "$LOGDIR"
 
-if [ -d "$BINDIR" ];
-then
-  CURRDIR=`dirname $0`
-  cp -r "$CURRDIR/bin" "$BINDIR"
-else
+if [ ! -d "$BINDIR" ];
   echo "Verzeichnis $BINDIR konnte nicht erstellt und Skripte nicht installiert werden"
   exit 1
 fi
 
+CURRDIR=`dirname $0`
+cp -r "$CURRDIR/bin" "$APPDIR"
+cp -r "$CURRDIR/js" "$APPDIR"
+cp -r "$CURRDIR/css" "$APPDIR"
+cp "$CURRDIR/status_ajax.html" "$APPDIR"
 
 CRONCMD="$BINDIR/logtemp 2>  $APPDIR/cron_errors > /dev/null"
 CRONJOB="*/5 *   * * *  $CRONCMD"
