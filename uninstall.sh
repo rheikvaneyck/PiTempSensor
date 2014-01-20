cf2fb869dfb7722aad875d11491b9530943e155f
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-01-20 22:46:25 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Uninstall Routine
 
+APPDIR="$HOME/PiTempSensor"
+BINDIR="$APPDIR/bin"
+
 CRONCMD="$BINDIR/logtemp 2> $APPDIR/cron_errors > /dev/null"
 
 echo "Entferne cronjob: $CRONCMD"
